67e2f2387ecaa8a3995871cc45064f7fdc16cc0a
Rodrigo <zinxrodrigo01@gmail.com>
2026-05-13 16:48:05 -0300

    feat:Colocando projeto no repositorio

---
+++ ProjetoExemploTop/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoExemploTop.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public string Nivel { get; set; } = "Operador"; // Admin, Gerente, Operador
+    }
+}
---
+++ ProjetoExemploTop/Repositorio/IProdutoRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoExemploTop.Models;
+
+namespace ProjetoExemploTop.Repositorio
+{
+    public interface IProdutoRepositorio
+    {
+        IEnumerable<Produto> ListarTodos();
+        Produto? ObterPorId(int id); 
+        void Adicionar(Produto produto);
+        void Atualizar(Produto produto); 
+        void Excluir(int id); 
+
+    }
+}
