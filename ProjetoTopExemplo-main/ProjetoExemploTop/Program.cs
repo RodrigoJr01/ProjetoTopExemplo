4470a75fdcb79d5ecae67ccb86c503d09ba912b5
Rodrigo <zinxrodrigo01@gmail.com>
2026-05-20 17:05:09 -0300

    feat:segundo commit

---
+++ ProjetoTopExemplo-main/ProjetoExemploTop/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using ProjetoExemploTop.Repositorio;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => {
+        options.LoginPath = "/Usuario/Logar"; // Onde o usuário cai se não estiver logado
+        options.AccessDeniedPath = "/Usuario/AcessoNegado"; // Se não tiver permissão
+    });
+
+// Registrando a Injeção de Dependência
+builder.Services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
+builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//IMPORTANTE ADICIONAR
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
